74b77e023fcc6ff36fcb298eb2cbe19d663aa7d6
u2pitchjami <u2pitchjami@gmail.com>
2024-11-21 13:41:54 +0100

    ajouts options

--- beets_manuel.sh
+++ beets_manuel.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ############################################################################## 
 #                                                                            #
-#	SHELL: !/bin/bash       version 1                                      #
+#	SHELL: !/bin/bash       version 2                                      #
 #									                                         #
 #	NOM: u2pitchjami						                                 #
 #									                                         #
 #	            							                                 #
 #									                                         #
-#	DATE: 17/11/2024	           				                             #
+#	DATE: 21/11/2024	           				                             #
 #								                                    	     #
 #	BUT: intégration manuelle des rejets Beets      #
 #									                                         #
@@ -29,8 +29,11 @@ tar -czf ${DIRSAV_BEETS}${BACKUP_BEETS}.tar.gz ${BASE_BEETS}
 echo -e "Sauvegarde compressée: \e[32m${BACKUP_BEETS}.tar.gz\e[0m\n" | tee -a "${LOG}"
 echo -e "sauvegarde réalisée" | tee -a "${LOG}"
 
+int_manuelle() {
+
 echo -e "Modification du fichier de conf" | tee -a "${LOG}"
-if [ -f $CONFIG_MANUEL ]; then
+#echo "${CONFIG_MANUEL}"
+if [ -f "${CONFIG_MANUEL}" ]; then
     mv "${CONFIG}" "${CONFIG_NORMAL}"
     mv "${CONFIG_MANUEL}" "${CONFIG}"
 fi
@@ -52,7 +55,7 @@ LIGNE=$(cat "${BEETSMANUEL}" | head -1 | tail +1)
 NOMLIGNE=$(echo "${LIGNE}" | cut -d "/" -f 6,7 )
 echo "Albums : ${NOMLIGNE} "| tee -a $LOG
 
-beet import -C --noincremental "${LIGNE}"
+beet import --noincremental "${LIGNE}"
 
 if [[ $? -eq "0" ]]; then
     echo -e "Traitement OK, ligne suivante" | tee -a "${LOG}"
@@ -72,3 +75,114 @@ fi
 echo "[`date`] - Voilà c'est fini, bisous" | tee -a $LOG
 
 rm TEMP
+}
+modif_mode() {
+if [ -f "${CONFIG_MANUEL}" ]; then
+    mv "${CONFIG}" "${CONFIG_NORMAL}"
+    mv "${CONFIG_MANUEL}" "${CONFIG}"
+    MODE="manuel"
+else
+    mv "${CONFIG}" "${CONFIG_MANUEL}"
+    mv "${CONFIG_NORMAL}" "${CONFIG}"
+    MODE="normal"
+fi
+
+echo -e "${BOLD}Mode ${MODE} activé ${NC}"  | tee -a $LOG
+sleep 3
+
+menu
+}
+
+update() {
+echo -e "${SAISPAS}${BOLD}Démarrage de la mise à jour de la base${NC}"
+
+beet update
+
+echo -e "${BOLD}Update terminée ${NC}"  | tee -a $LOG
+sleep 3
+
+menu
+}
+
+import_auto() {
+echo -e "${SAISPAS}${BOLD}Démarrage de l'importation auto de la base ${NC}"
+
+if [ -f $CONFIG_NORMAL ]; then
+    mv "${CONFIG}" "${CONFIG_MANUEL}"
+    mv "${CONFIG_NORMAL}" "${CONFIG}"
+fi
+
+beet import "${BASE}"
+
+echo -e "${BOLD}importation auto terminée ${NC}"  | tee -a $LOG
+sleep 3
+echo -e "[`date`] - Extraction Beets des albums à importer manuellement :" | tee -a $LOG
+grep "^skip" /home/pipo/bin/logs/beets/beet.log | cut -c6- >> /home/pipo/bin/recap_music/beets_manuel.txt
+echo "" > /home/pipo/bin/logs/beets/beet.log
+echo -e "Fichier disponible dans le dossier recap_music :" | tee -a $LOG
+
+menu
+}
+
+move() {
+echo -e "${SAISPAS}${BOLD}Démarrage du déplacements des fichiers mal placés ${NC}"
+
+if [ -f $CONFIG_NORMAL ]; then
+    mv "${CONFIG}" "${CONFIG_MANUEL}"
+    mv "${CONFIG_NORMAL}" "${CONFIG}"
+fi
+
+beet move
+
+echo -e "${BOLD}déplacements terminés ${NC}"  | tee -a $LOG
+sleep 3
+
+menu
+}
+
+menu() {
+
+NBBEETSMANUEL=$(cat "${BEETSMANUEL}" | wc -l)
+if [ -f "${CONFIG_MANUEL}" ]; then
+    CHANGE_MODE="manuel"
+else
+    CHANGE_MODE="normal"
+fi
+
+echo -e "${SAISPAS}${BOLD}Que souhaites tu faire ?${NC}"
+echo "1) - Traiter les intégrations manuelles ("${NBBEETSMANUEL}" en attente)"
+echo "2) - Passer en mode : "${CHANGE_MODE}""
+echo "3) - Réaliser une update de la base"
+echo "4) - Réaliser une intégration automatique"
+echo "5) - Mouvementer les fichiers de la base"
+echo "6) - Quitter"
+read MENU
+
+case $MENU in
+
+1)
+    int_manuelle
+    ;;
+2)
+    modif_mode
+    ;;
+3)
+    update
+    ;;
+4)
+    import_auto
+    ;;
+5)
+    move
+    ;;
+6)
+    quitter
+    ;;
+esac
+}
+
+quitter() {
+echo "c'est fini bisous" | tee -a "${LOGS}"
+}
+
+menu
